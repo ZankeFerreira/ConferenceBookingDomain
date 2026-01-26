25075a2b7bbbc579ee6f33dfac6b0599fa9429a6
ZankeFerreira <zanke.ferreira1229@gmail.com>
2026-01-26 23:54:18 +0200

    Added Conference room domian that tracks which uses a roomnumber, capacity, the room status to store rooms

--- obj/Debug/net8.0/ConferenceBookingDomain.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConferenceBookingDomain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245b49ebc8905090f759d6db6a967df5f486fc1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ec1763961fdbce38a68ec96ff7eba76a3cb98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
