b8285aada4fb1360450a010daf5cf2a36feabb2a
ZankeFerreira <zanke.ferreira1229@gmail.com>
2026-02-07 18:32:28 +0200

    Added authentication controller

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController: ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly TokenService _tokenService;
+
+        public AuthController(UserManager<ApplicationUser> userManager, TokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("loging")]
+        public async Task <IActionResult> Login(LoginDto dto)
+        {
+            var user = await _userManager.FindByNameAsync(dto.Username);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, dto.Password))
+            {
+                return Unauthorized();
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var token = _tokenService.GenerateToken(user, roles);
+
+            return Ok(new {token});
+        }
+    }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0907b0c709c380c37836655458875a958a25a301")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926aa42e0ca8b07b8ff549be259f62d80216c424")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
